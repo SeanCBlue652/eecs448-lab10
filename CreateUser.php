47bbc02bc63545a1573d01000810387ddf32e134
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-12-06 20:26:38 -0600

    Update CreateUser.php

--- CreateUser.php
+++ CreateUser.php
@@ -28,12 +28,12 @@ if ($result = $mysqli->query($query)) {
 if ($userExists) {
     echo "User {$username} cannot be created because they already exist.\n";
 } else {
-    $insertString = "INSERT INTO Users (user_id) VALUES ({$username})";
+    $insertString = "INSERT INTO Users (user_id) VALUES (\'{$username}\')";
     if ($mysqli->query($insertString)) {
         echo "User {$username} has been successfully created.\n";
         $result->free();
     } else {
-        echo "Error creating user {$username}: %s\n", $mysqli->error;
+        echo "Error creating user {$username}: ", $mysqli->error, "\n";
         $result->free();
     }
     
