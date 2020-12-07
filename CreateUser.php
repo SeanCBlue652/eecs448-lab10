0f4a8bbf3efe22b05e6eb92faa32b698f1b21f43
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-12-06 20:07:30 -0600

    update

--- CreateUser.php
+++ CreateUser.php
@@ -1,4 +1,7 @@
 <?php
+
+$username = $_POST["input"];
+
 $mysqli = new mysqli("mysql.eecs.ku.edu", "seancunningha", "fohNuph4",
 "seancunningha");
 /* check connection */
@@ -6,15 +9,30 @@ if ($mysqli->connect_errno) {
  printf("Connect failed: %s\n", $mysqli->connect_error);
  exit();
 }
-$query = "SELECT * FROM Users";
+
+$userExists = false;
+
+$query = "SELECT {$username} FROM Users";
 if ($result = $mysqli->query($query)) {
  /* fetch associative array */
  while ($row = $result->fetch_assoc()) {
- printf ("%s\n", $row["user_id"]);
+ if ($row["user_id"] == $username) {
+     $userExists = true;
+ break;
+ }
  }
  /* free result set */
  $result->free();
 }
+
+if ($userExists) {
+    echo "User {$username} cannot be created because they already exist.\n";
+} else {
+    $insertString = "insert into Users (user_id) values ({$username})"
+    $mysqli->query($insertString);
+    echo "User {$username} has been successfully created.\n";
+}
+
 /* close connection */
 $mysqli->close();
 ?>
