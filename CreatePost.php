aa3a50a10b04f906326d2bf1d0fe63fe9725bda1
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-12-06 20:40:42 -0600

    update

---
+++ CreatePost.php
@@ -0,0 +1,50 @@
+<?php
+
+$username = $_POST["username"];
+$userpost = $_POST["input"];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "seancunningha", "fohNuph4",
+"seancunningha");
+/* check connection */
+if ($mysqli->connect_errno) {
+ printf("Connect failed: %s\n", $mysqli->connect_error);
+ exit();
+}
+
+$userExists = false;
+
+$query = "SELECT {$username} FROM Users";
+if ($result = $mysqli->query($query)) {
+ /* fetch associative array */
+ while ($row = $result->fetch_assoc()) {
+ if ($row["user_id"] == $username) {
+     $userExists = true;
+ break;
+ }
+ }
+ /* free result set */
+ $result->free();
+}
+
+if ($userpost == "" || $userpost == null) {
+    echo "Post cannot be blank.\n";
+} else {
+    if ($userExists) {
+        $insertString = "INSERT INTO Posts (author_id, content) VALUES ('{$username}', '{$userpost}')";
+        if ($mysqli->query($insertString)) {
+            echo "Post {$userpost} has been successfully added for the user: {$username}.\n";
+            $result->free();
+        } else {
+            echo "Error creating post: ", $mysqli->error, "\n";
+            $result->free();
+        }
+    } else {
+        echo "Post cannot be created for the user {$username} because that user does not exist.\n";  
+    }
+}
+
+
+
+/* close connection */
+$mysqli->close();
+?>
